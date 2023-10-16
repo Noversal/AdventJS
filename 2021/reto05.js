4412885410ff9040604f5f51d101c95ec6272b9d
Franco Dionofrio <83983474+Noversal@users.noreply.github.com>
2023-10-16 02:21:30 +0000

    different way to solve challenge five

--- 2021/reto05.js
+++ 2021/reto05.js
@@ -1,10 +1,8 @@
 export default function daysToXmas(date) {
-    const xmas_date = new Date('Dec 25 2021')
-    const xmas_miliseconds = Date.parse(xmas_date)
-    const milisecond_date = Date.parse(date)
-    const diffrent_days_miliseconds = xmas_miliseconds - milisecond_date
-    const miliseconds_day = 8.64e+7
-    const days_with_decimal = diffrent_days_miliseconds / miliseconds_day
-    const days = Math.ceil(days_with_decimal)
-    return days
-}
+    const xmas = new Date('Dec 25, 2021').getTime()
+    const day = date.getTime()
+    const diffTime = xmas - day
+    const miliseconds_day = 86_400_000
+    const diffDay = diffTime / (miliseconds_day)  
+    return Math.ceil(diffDay)
+  }
